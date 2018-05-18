6cabfad52dd5b8d57006d7bf26e58242d5235062
Jason Spangler <jason.spangler@descendentstudios.com>
2018-05-18 18:21:28 -0500

    Version number fix

--- src/java/com/wumple/palimpsest/Main.java
+++ src/java/com/wumple/palimpsest/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
 	public static final String MODID = "palimpsest";
 	public static final String MODNAME = "Palimpsest";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.1.2";
 
 	@Instance
 	public static Main instance = new Main();
