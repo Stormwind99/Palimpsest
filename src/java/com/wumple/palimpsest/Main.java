f2c176351931f524433a0e391124e624f482766c
Jason Spangler <jason.spangler@descendentstudios.com>
2018-05-18 17:17:22 -0500

    Update to 1.12.2

--- src/java/com/wumple/palimpsest/Main.java
+++ src/java/com/wumple/palimpsest/Main.java
@@ -1,42 +1,27 @@
 package com.wumple.palimpsest;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+//import net.minecraft.init.Items;
+//import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+//import net.minecraftforge.fml.common.registry.GameRegistry;
 
-import net.minecraft.world.World;
-import net.minecraft.item.ItemStack;
-import net.minecraft.init.Items;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-
-@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION, updateJSON = "https://raw.githubusercontent.com/Stormwind99/Palimpsest/master/update.json")
 public class Main {
 
-	public static final String MODID = "Palimpsest";
+	public static final String MODID = "palimpsest";
 	public static final String MODNAME = "Palimpsest";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.1";
 
 	@Instance
 	public static Main instance = new Main();
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.writable_book, 1),
-				new Object[] { new ItemStack(Items.writable_book, 1), new ItemStack(Items.redstone, 1), });
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.book, 1),
-				new Object[] { new ItemStack(Items.enchanted_book, 1), new ItemStack(Items.redstone, 1), });
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.map, 1),
-				new Object[] { new ItemStack(Items.map, 1), new ItemStack(Items.redstone, 1), });
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.book, 1),
-				new Object[] { new ItemStack(Items.written_book, 1), new ItemStack(Items.redstone, 1), });
-
-		filledMapPalimpsestRecipe.init();
 	}
 
 	@EventHandler
