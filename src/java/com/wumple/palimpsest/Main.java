a7787fbfe106dc48e0ed864f71a9cf374e3f53ba
Jason Spangler <jason.spangler@descendentstudios.com>
2018-05-19 15:37:10 -0500

    Move all identifiers and version ids into one file, following Botania example

--- src/java/com/wumple/palimpsest/Main.java
+++ src/java/com/wumple/palimpsest/Main.java
@@ -1,21 +1,14 @@
 package com.wumple.palimpsest;
 
-//import net.minecraft.init.Items;
-//import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-//import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION, updateJSON = "https://raw.githubusercontent.com/Stormwind99/Palimpsest/master/update.json")
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION, dependencies = LibMisc.DEPENDENCIES, updateJSON=LibMisc.UPDATEJSON)
 public class Main {
-
-	public static final String MODID = "palimpsest";
-	public static final String MODNAME = "Palimpsest";
-	public static final String VERSION = "1.1.2";
 
 	@Instance
 	public static Main instance = new Main();
